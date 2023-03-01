22b7dbc53ff553aaf49d0cdb9aee33ad32df9ff1
fon_neumann <alexdobr08@mail.ru>
2023-03-02 01:17:30 +0300

    Added GUI for the game. Buttons are connected to js code via event listeners. Game restarts after score 5.

--- script.js
+++ script.js
@@ -1,71 +1,100 @@
+const buttons = document.querySelectorAll('.button');
+const resultsDiv = document.querySelector('#results');
+const scoreDiv = document.querySelector('#score');
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    let answer
-    switch (Math.round(Math.random() * 2)) {
-      case 0:
-        answer = "rock";
-        break;
-      case 1:
-        answer = "paper";
-        break;
-      case 2:
-        answer = "scissors";
-    }
-    return answer
+  let answer;
+  switch (Math.floor(Math.random() * 3)) {
+    case 0:
+      answer = "rock";
+      break;
+    case 1:
+      answer = "paper";
+      break;
+    case 2:
+      answer = "scissors";
+      break;
   }
-  
-  function oneRound(playerSelection, computerSelection) {
-    switch (playerSelection) {
-      case "rock":
-        switch (computerSelection) {
-          case "rock":
-            return "Draw";
-          case "paper":
-            return "You lose";
-          case "scissors":
-            return "You win";
-        }
-      case "paper":
-        switch (computerSelection) {
-          case "rock":
-            return "You win";
-          case "paper":
-            return "Draw";
-          case "scissors":
-            return "You lose";
-        }
-      case "scissors":
-        switch (computerSelection) {
-          case "rock":
-            return "You lose";
-          case "paper":
-            return "You win";
-          case "scissors":
-            return "Draw";
-        }
-    }
-  }
-  
-  function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-      let computerSelection = getComputerChoice()
-      let playerSelection = prompt("Enter your bid").toLowerCase()
-      let result = oneRound(playerSelection, computerSelection)
-      console.log(result)
-      if (result === "You win") {
-        playerScore++;
-      } else if (result === "You lose") {
-        computerScore++;
+  return answer;
+}
+
+function playRound(playerSelection) {
+  const computerSelection = getComputerChoice();
+  let result;
+
+  switch (playerSelection) {
+    case "rock":
+      switch (computerSelection) {
+        case "rock":
+          result = "Draw";
+          break;
+        case "paper":
+          result = "You lose";
+          computerScore++;
+          break;
+        case "scissors":
+          result = "You win";
+          playerScore++;
+          break;
       }
-    }
-    if (playerScore > computerScore) {
-      return "You win the game!";
-    } else if (playerScore < computerScore) {
-      return "You lose the game.";
-    } else {
-      return "It's a tie!";
-    }
+      break;
+    case "paper":
+      switch (computerSelection) {
+        case "rock":
+          result = "You win";
+          playerScore++;
+          break;
+        case "paper":
+          result = "Draw";
+          break;
+        case "scissors":
+          result = "You lose";
+          computerScore++;
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computerSelection) {
+        case "rock":
+          result = "You lose";
+          computerScore++;
+          break;
+        case "paper":
+          result = "You win";
+          playerScore++;
+          break;
+        case "scissors":
+          result = "Draw";
+          break;
+      }
+      break;
   }
-  
-  console.log(game())
+
+  resultsDiv.textContent = `You chose ${playerSelection}, computer chose ${computerSelection}. ${result}`;
+  scoreDiv.textContent = `Score: Player ${playerScore} - ${computerScore} Computer`;
+
+  if (playerScore === 5 || computerScore === 5) {
+    if (playerScore > computerScore) {
+      alert("You win the game!");
+    } else {
+      alert("You lose the game.");
+    }
+    resetGame();
+  }
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  scoreDiv.textContent = `Score: Player ${playerScore} - ${computerScore} Computer`;
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    playRound(button.dataset.selection);
+  });
+});
+
+ 
